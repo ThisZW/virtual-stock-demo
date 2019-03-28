da3691bdb41851aa0cf525b5bc4a0f5462c0c2ea
ThisZW <everbread1234@gmail.com>
2019-03-28 01:34:02 -0400

    jwt token setting up

---
+++ server/auth/index.js
@@ -0,0 +1,23 @@
+
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "THISISSOMERANDOMSECRETKEYJUSTFORTHISCUTEANDTINYJSONWEBTOKEN";
+
+const strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = users.find(user => user.id === jwt_payload.id);
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { inspect } = require('util')
+const router = express.Router();
+const db = require('../db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/test', async (req, res, next) => {
+  res.json({'user': await getUser()})
 });
+
+const getUser = async() => {
+  const dbres = db.getDB()
+  let res 
+  try { 
+    res = await dbres.collection('users').findOne({email: 'testEmail'})
+    return res
+  } catch (e) {
+    throw e
+  }
+}
 
 module.exports = router;
